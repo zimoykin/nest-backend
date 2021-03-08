03654ef0e958187432d795d582d509f61af703ea
Dmitry <zimoykin@gmail.com>
2021-03-08 18:37:13 +0300

    #%#$@@#

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,16 +2,16 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 require('dotenv').config();
 
-const { DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST } = process.env;
+const { DB_PORT, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, DB_HOST } = process.env;
 console.log (__dirname )
 export const config: TypeOrmModuleOptions = {
   name: 'default',
   type: 'postgres',
   host: DB_HOST,
   port: parseInt(DB_PORT),
-  username: DB_USERNAME,
-  password: DB_PASSWORD,
-  database: DB_DATABASE,
+  username: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  database: POSTGRES_DB,
   synchronize: false,
   entities: [
     __dirname + "/src/model/*.entity{.ts,.js}"

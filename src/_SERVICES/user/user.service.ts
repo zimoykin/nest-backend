0789da1e9f635703f30963d24c45c9d45c1a30f3
Dmitry <zimoykin@gmail.com>
2021-03-30 23:20:41 +0300

    @

--- src/_SERVICES/user/user.service.ts
+++ src/_SERVICES/user/user.service.ts
@@ -1,8 +1,13 @@
-import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import {
   UserAccess,
   UserInputDto,
-  UserRefreshToken
+  UserRefreshToken,
 } from '../../_MODEL/_DTO/user.dto';
 import { User } from '../../_MODEL/user.entity';
 import * as bcrypt from 'bcrypt';
@@ -12,50 +17,44 @@ import { Roles } from '../../_UTILS/enums/roles';
 
 const jwt = require('jsonwebtoken');
 
-
 @Injectable()
-export class UsersService extends ModelService(User, User.relations){
-
+export class UsersService extends ModelService(User, User.relations) {
   login(email: string, password: string): Promise<User> {
-
-    return this.readRaw({ email: email })
-    .then((user) => {
+    return this.readRaw({ email: email }).then((user) => {
       if (user) {
-      return bcrypt.compare(password, user.password).then((ismatch) => {
-        if (ismatch) {
-          return user;
-        } else {
-          throw new HttpException(
-            Error('password incorrect'),
-            HttpStatus.BAD_REQUEST,
-          );
-        }
-      });
-    } else throw new NotFoundException()
-  });
+        return bcrypt.compare(password, user.password).then((ismatch) => {
+          if (ismatch) {
+            return user;
+          } else {
+            throw new HttpException(
+              Error('password incorrect'),
+              HttpStatus.BAD_REQUEST,
+            );
+          }
+        });
+      } else throw new NotFoundException();
+    });
   }
 
   refresh(refresh: UserRefreshToken) {
     return new Promise((resolve) => {
-      this.read({refresh}).then((user) => {
+      this.read({ refresh }).then((user) => {
         resolve(user);
       });
     });
   }
 
-  async createOne(payload: UserInputDto): Promise<User>{
-
+  async createOne(payload: UserInputDto): Promise<User> {
     let user = await this.repository.create({
       isActive: true,
       username: payload.username,
       email: payload.email,
       password: payload.password,
       gender: Gender[payload.gender],
-      role: Roles[payload.role]
-    })
-   
-    return this.repository.save(user);  
-  
+      role: Roles[payload.role],
+    });
+
+    return this.repository.save(user);
   }
 
   toAccess(user: User): Promise<UserAccess> {
@@ -66,10 +65,11 @@ export class UsersService extends ModelService(User, User.relations){
       refreshToken: jwt.sign({ id: user.id }, jwt_secret, { expiresIn: '72h' }),
     };
 
-    return this.update(user.id, { refreshToken: access.refreshToken })
-      .then((val) => {
+    return this.update(user.id, { refreshToken: access.refreshToken }).then(
+      (val) => {
         return access;
-      });
+      },
+    );
   }
 
   async checkRefToken(ref: string): Promise<User> {
@@ -77,16 +77,19 @@ export class UsersService extends ModelService(User, User.relations){
 
     return new Promise((resolve, reject) => {
       jwt.verify(ref, jwt_secret, async (err, payload) => {
-        if (err) reject(err);
-        let user = await this.read({ id: payload.id });
-        if (!user) reject('user not found');
-        resolve(user);
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else {
+          let user = await this.readRaw({ id: payload.id });
+          if (!user) reject('user not found');
+          resolve(user);
+        }
       });
     });
   }
 
   saveFile(input: any, req: any): Promise<string> {
-    return
+    return;
   }
-
 }
